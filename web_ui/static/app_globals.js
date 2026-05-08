7c32416c16a34485d21b9cd4b6491ea6406e666e
inn1983-png <inn1983@gmail.com>
2026-05-08 08:37:52 +0800

    Expose app globals for result workbench plugins

---
+++ web_ui/static/app_globals.js
@@ -0,0 +1,12 @@
+(() => {
+  // app.js uses top-level const/function declarations. Later UI plugins need a
+  // stable window bridge, but we do not want to rewrite the whole app.js file.
+  try {
+    if (typeof state !== "undefined") window.state = state;
+    if (typeof MODULE_NAMES !== "undefined") window.MODULE_NAMES = MODULE_NAMES;
+    if (typeof STAGE_NAMES !== "undefined") window.STAGE_NAMES = STAGE_NAMES;
+    if (typeof api !== "undefined") window.api = api;
+    if (typeof previewFile !== "undefined") window.previewFile = previewFile;
+    if (typeof switchTab !== "undefined") window.switchTab = switchTab;
+  } catch (_) {}
+})();
